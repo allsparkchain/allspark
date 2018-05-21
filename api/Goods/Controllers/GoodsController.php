201ffc00f5883d1884bc6214ca6f84cdfe5c8085
shixiaoyi <729718933@qq.com>
2018-05-21 09:02:17 +0800

    no message

--- api/Goods/Controllers/GoodsController.php
+++ api/Goods/Controllers/GoodsController.php
@@ -287,4 +287,250 @@ class GoodsController
         }
     }
 
+    /**
+     * 商品详情页面
+     * @Get("/previewDetail", as="s_goods_previewDetail")
+     */
+    public function previewDetail(Request $request) {
+        $id = $request->get('id');
+        if(!$id || is_null($id) || !intval($id)){
+            return redirect(route('s_goods_lists'));
+        }
+        try {
+            $post = Curl::post('/product/getGoodsShow', $arr = [
+                'product_id' => $id,
+                'status' =>'1,2'
+            ]);
+        } catch (ApiException $e) {
+
+        }
+
+        if(empty($post['data'])){
+            $status = 404;
+            if (view()->exists("errors.{$status}")){
+                return response()->view("errors.{$status}", [], $status);
+            }
+
+            return redirect(route('s_goods_lists'));
+        }
+
+
+        $post['data']['percent_commission'] = 0;
+        $post['data']['percent_account'] = 0;
+
+        if($post['data']['percent_key'] && is_array($post['data']['percent_key'])){
+            foreach ($post['data']['percent_key'] as $k => $v){
+                if($v['type']==3){
+                    $post['data']['percent_commission'] = $v['contents']['percent'];
+                    $post['data']['percent_account'] = $v['contents']['account'];
+                }
+            }
+        }
+        $wxHost = config('params.wx_host');
+        $code = Curl::post('/weixin/createCode');
+        $jzstate  = ($code['data']);
+        \Session::put("pc_jzstate", $jzstate);
+        \Cache::put("wxdl_".$jzstate, $jzstate,30);
+
+        $user = \Auth::getUser()?\Auth::getUser()->getAuthIdentifier(): '';
+
+
+        try {
+            //统计 查看页面次数
+            Curl::post('/weixin/addProductQuantity', $arr = [
+                'product_id' => $id,
+                'uid' => $user ? $user : 0
+            ]);
+        } catch (ApiException $e) {
+
+        }
+
+
+        $ori_path = $post['data']['img_path'];
+        $extension = explode('/',$ori_path);
+        $name = $extension[count($extension) -1];
+
+        $new_path325 = md5($name.'_325_325');
+        $post['data']['new_path325'] = str_replace($name,$new_path325,$ori_path);
+
+        return view("Goods.previewDetail")
+            ->with('user', $user)
+            ->with('pc_jzstate', $jzstate)
+            ->with('wxHost',$wxHost)
+            ->with('res',$post['data']);
+    }
+
+    /**
+     * 文章边界页面
+     * @Get("/editor", as="s_goods_edit")
+     * @Middleware("auth")
+     */
+    public function editor(Request $request) {
+        $id = $request->get('id');
+        $article_id = $request->get('article_id');
+
+        if($article_id){
+            $article = Curl::post('/article/getArticle',['articleid'=>$article_id]);
+            $id = $article['data']['product_id'];
+        }
+        try {
+            $post = Curl::post('/product/getGoodsShow', $arr = [
+                'product_id' => $id
+            ]);
+        } catch (ApiException $e) {
+
+        }
+
+        try {
+//            $article_category = getRedisData('articleCategoryWrite'.$id,'/industryCategory/getCategoryWithIndustryList',['type'=>2]);
+//            $article_category = Curl::post('/industry/getLists', ['status'=>1,'type'=>2]);
+            $article_category = getRedisData('articleCategoryWrite'.$id,'/industryCategory/getIndustryCategoryListWithIndustry',['type'=>2]);
+            $article_category = $article_category['data']['data'];
+//            var_dump($article_category);
+        } catch (ApiException $e) {
+            $article_category = [];
+        }
+
+        return view("Goods.editor")->with('res',$post['data'])->with('article_category',$article_category);
+    }
+
+    /**
+     * 文章边界页面
+     * @Get("/preview", as="s_goods_preview")
+     */
+    public function preview(Request $request) {
+
+        return view("Goods.preview");
+
+    }
+
+    /**
+     * 文章预览
+     * @Get("/success", as="s_goods_success")
+     */
+    public function success(Request $request) {
+
+        return view("Goods.success");
+
+    }
+
+
+
+    /**
+     * 商品列表页面Ajax
+     * @Post("/getGoodsList", as="s_goods_getGoodsList")
+     */
+    public function getGoodsList(Request $request) {
+        try {
+
+            $page = $request->get("page",1);
+            $pagesize = $request->get("pagesize",10);
+            $category_id = $request->get("category_id",0);
+            $region_id = $request->get("region_id",0);
+            $arr = [
+                'page' => $page,
+                'pagesize' => $pagesize,
+            ];
+            if($category_id >0 ){
+                $arr['category_id']= $category_id;
+            }
+            if($region_id >0 ){
+                $arr['region_id']= $region_id;
+            }
+            $post = getRedisData('getGoodsList'.md5(json_encode($request->all())),'/product/getGoodsShowList',$arr);
+
+            foreach ($post['data']['data'] as $key =>$val){
+                if($val['percent_key'] && is_array($val['percent_key'])){
+                    foreach ($val['percent_key'] as $k => $v){
+                        if($v['type']==3){
+                            $post['data']['data'][$key]['percent_commission'] = $v['contents']['percent'];
+                            $post['data']['data'][$key]['percent_account'] = $v['contents']['account'];
+                        }
+                    }
+                }
+                $post['data']['data'][$key]['synopsis'] = $val['synopsis']?$val['synopsis']:'';
+            }
+
+            return new JsonResponse($post);
+        } catch (ApiException $e) {
+            return new JsonResponse([
+                "status"=>$e->getCode(),
+                "message"=>$e->getMessage(),
+            ]);
+        }
+    }
+
+    /**
+     * 商品分类列表页面Ajax
+     * @Post("/getCategorList", as="s_goods_getCategorList")
+     */
+    public function getCategorList(Request $request) {
+        try {
+            $arr = ['status'=>1,'type'=>1];
+//            $post = Curl::post('/productCategory/getProductCategoryList', ['status'=>1]);
+//            $post = Curl::post('/industry/getLists',$arr );
+//            $post = getRedisData('GoodsgetCategoryList'.md5(json_encode($request->all())),'/industry/getLists',$arr);
+            $arr = ['type'=>1,'status'=>1];
+            $post = getRedisData('GoodsgetCategoryList'.md5(json_encode($request->all())),'/industryCategory/getLists',$arr);
+
+            return new JsonResponse($post);
+        } catch (ApiException $e) {
+            return new JsonResponse([
+                "status"=>$e->getCode(),
+                "message"=>$e->getMessage(),
+            ]);
+        }
+    }
+
+    /**
+     *
+     * @Post("/articleAdd", as="s_article_add")
+     * @Middleware("auth")
+     */
+    public function articleAdd(Request $request) {
+        try {
+
+            $paramer['id'] = $request->get('id', 0);
+            $paramer['name'] = $request->get('name');
+            $paramer['content'] = $request->get('content');
+            $paramer['article_product_id'] = $request->get('product_id');
+            $paramer['spiltway'] = 3;
+            $paramer['author'] = $this->getUserId();
+
+            $paramer['article_category_id'] = $request->get('article_category_id',0);
+
+            $status = $request->get('status');
+
+
+            if($status==1){
+                $paramer['status'] = 3;//发布
+            }else{
+                $paramer['status'] = 5;//草稿
+            }
+
+            if($paramer['id']){
+                $data = Curl::post('/article/editArticle',
+                    $paramer
+                );
+            }else{
+                $data = Curl::post('/article/add',
+                    $paramer
+                );
+            }
+
+
+
+            if($data['status']==200){
+                $phpPath = config('params.php_path');
+                shell_exec("nohup ".$phpPath.' '.public_path('../').'artisan command:ossUploadStript '.$data['data'].' >> /tmp/out.file 2>&1  &');
+            }
+
+            return new JsonResponse($data);
+        } catch (ApiException $e) {
+            return new JsonResponse([
+                "status"=>$e->getCode(),
+                "message"=>$e->getMessage(),
+            ]);
+        }
+    }
 }
--- api/Users/Controllers/UserController.php
+++ api/Users/Controllers/UserController.php
@@ -1039,4 +1039,245 @@ class UserController
         }
         return false;
     }
+
+    /**
+     * 好友邀请页面
+     * @Get("/inviteData", as="s_user_inviteData")
+     */
+    public function inviteData() {
+        //https://wx.pugongying.link/Share/index?invite_code=f47d4374&handurl=https://thirdwx.qlogo.cn/mmopen/vi_32/DYAIOgq83eqLEBIHbbU5FoU3xazWaeoBQ4ozwpGoslB7jGS8Bw03zdsqCY7LPrMTIYCDxbUICLWTicIkavBgYhA/142
+
+        $arr = [
+            'headImgurl'=>\Auth::getUser()->getHeadImgurl(),
+            'realname' =>\Auth::getUser()->getUserNickname()
+        ];
+
+        \Cache::forever($this->getRecommendCode(),$arr);
+
+        $wxHost = config('params.wx_host');
+        $fxurl = $wxHost.'Share/share?invite_code='.$this->getRecommendCode();
+
+
+        return view("User.inviteData")->with("code", $this->getRecommendCode())->with('fxurl',$fxurl);
+    }
+
+    /**
+     * 账户设置页面
+     * @Get("/accountSetting", as="s_user_accountSetting")
+     */
+    public function accountSetting() {
+        return view("User.accountSetting")->with('mobile',$this->getUserName());
+    }
+
+    /**
+     * 我的资讯页面
+     * @Get("/articleList", as="s_user_articleList")
+     */
+    public function articleList() {
+        return view("User.articleList");
+    }
+
+
+    //apiPost-------------
+    /**
+     * 获取账户总览数据
+     * @Post("/getAccountInfo", as="s_user_getAccountInfo")
+     */
+    public function getAccountInfo(Request $request) {
+        try {
+            $uid = $this->getUserId();
+
+            $page = $request->get("page",1);
+            $pagesize = $request->get("pagesize",10);
+            $post = Curl::post('/user/userAccountPage', $arr = [
+                'uid'=> $uid,
+                'page' =>$page,
+                'pagesize'=>$pagesize,
+            ]);
+
+            return new JsonResponse($post);
+        } catch (ApiException $e) {
+            return new JsonResponse([
+                "status"=>$e->getCode(),
+                "message"=>$e->getMessage(),
+            ]);
+        }
+    }
+
+    /**
+     * 获取账户页面图表数据
+     * @Post("/getAccountInfoFlow", as="s_user_getAccountInfoFlow")
+     */
+    public function getAccountInfoFlow(Request $request) {
+        try {
+            $uid = $this->getUserId();
+            $seetype = $request->get("seetype",1);
+            /////
+            try{
+                $records = Curl::post('/user/getUserDayDraw',['uid'=>$uid,'seetype'=>$seetype]);
+            }catch (ApiException $e){
+                $records = [];
+            }
+//            if(isset($records['data']) && $records['data']['count'] >0){
+//                var_dump($records['data']);die;
+//                $info = [];
+//                $j = 0;
+//                for ($i=6;$i>=0;$i--){
+//                    $info[$j]['new_time'] = date('Y.m.d',strtotime('-'.$i.' day'));
+//                    $info[$j]['account'] = 0;
+//                    foreach ($records['data']['data'] as $key => $value){
+//                        if( date('Y.m.d',($value['add_time'])) ==  $info[$j]['new_time']){
+//                            $info[$j]['account'] = $value['account'];
+//                        }
+//                    }
+//                    $j++;
+//                }
+//                $records['data']['draw'] = $info;
+//            }else{
+//                //生成空的数据
+//                $now = time();
+//                $null_arr = [];
+//                for ($i = 1 ; $i<=7 ;$i++){
+//                    $null_arr[] = array('new_time'=>date('Y.m.d',strtotime('-'.$i.' day',$now)),'account'=>0);
+//                }
+//                $records['data']['draw'] = $null_arr;
+//            }
+            return new JsonResponse($records);
+        } catch (ApiException $e) {
+            return new JsonResponse([
+                "status"=>$e->getCode(),
+                "message"=>$e->getMessage(),
+            ]);
+        }
+    }
+
+    /**
+     * 获取收益结算数据接口查询
+     * @Post("/getCommissionSettlement", as="s_user_getCommissionSettlement")
+     */
+    public function getCommissionSettlement(Request $request) {
+        try {
+            $uid = $this->getUserId();
+            $seetype = $request->get("seetype");
+            $page = $request->get("page",1);
+            $pagesize = $request->get("pagesize",10);
+            $post = Curl::post('/user/getUserCommissionRecordPage', $arr = [
+                'uid'=> $uid,
+                'seetype'=>$seetype,
+                'page' =>$page,
+                'pagesize'=>$pagesize,
+            ]);
+            //var_dump($post);die;
+            return new JsonResponse($post);
+        } catch (ApiException $e) {
+
+            return new JsonResponse([
+                "status"=>$e->getCode(),
+                "message"=>$e->getMessage(),
+            ]);
+        }
+    }
+
+    /**
+     * 获取收益明细数据接口查询
+     * @Post("/getAccountCommissionSettlementDetail", as="s_user_getAccountCommissionSettlementDetail")
+     */
+    public function getAccountCommissionSettlementDetail(Request $request) {
+        try {
+            $uid = $this->getUserId();
+            $today = $request->get("today",-1);//1,-1
+            $startDay = $request->get("startDay",'');
+            $endDay = $request->get("endDay",'');
+            $page = $request->get("page",1);
+            $pagesize = $request->get("pagesize",10);
+
+            if(strlen($startDay)>0 && strlen($endDay)>0){
+                if(strtotime($startDay) > strtotime($endDay)){
+                    return new JsonResponse([
+                        "status"=>'309',
+                        "message"=>'日期选择不合法',
+                    ]);
+                }
+            }
+            $post = Curl::post('/user/settlementDetail', $arr = [
+                'uid'=> $uid,
+                'today'=>$today,
+                'startDay'=>$startDay,
+                'endDay'=>$endDay,
+                'page' =>$page,
+                'pagesize'=>$pagesize,
+            ]);
+            return new JsonResponse($post);
+        } catch (ApiException $e) {
+
+            return new JsonResponse([
+                "status"=>$e->getCode(),
+                "message"=>$e->getMessage(),
+            ]);
+        }
+    }
+
+    /**
+     * 获取推广数据接口查询1
+     * @Post("/getSpreadData", as="s_user_getSpreadData")
+     */
+    public function getSpreadData(Request $request) {
+        try {
+            $uid = $this->getUserId();
+
+            //type2 测试数据显示
+//            $uid = 18;
+
+
+
+            $page = $request->get("page",1);
+            $pagesize = $request->get("pagesize",10);
+            $post = Curl::post('/user/accountSpreadData', $arr = [
+                'uid'=> $uid,
+                'page' =>$page,
+                'pagesize'=>$pagesize,
+            ]);
+            //var_dump($post);die;
+            return new JsonResponse($post);
+        } catch (ApiException $e) {
+
+            return new JsonResponse([
+                "status"=>$e->getCode(),
+                "message"=>$e->getMessage(),
+            ]);
+        }
+    }
+
+    /**
+     * 获取推广 订单 数据接口查询2
+     * @Post("/getSpreadDataDetail", as="s_user_getSpreadDataDetail")
+     */
+    public function getSpreadDataDetail(Request $request) {
+        try {
+            $uid = $this->getUserId();
+
+            //type2 测试数据显示
+//            $uid = 18;
+
+
+            $spread_id = $request->get("spreadid",1);
+            $page = $request->get("page",1);
+            $pagesize = $request->get("pagesize",10);
+            $post = Curl::post('/user/accountSpreadDataDetail', $arr = [
+                'uid'=> $uid,
+                'spread_id'=>$spread_id,
+                'page' =>$page,
+                'pagesize'=>$pagesize,
+            ]);
+            //var_dump($post);die;
+            return new JsonResponse($post);
+        } catch (ApiException $e) {
+
+            return new JsonResponse([
+                "status"=>$e->getCode(),
+                "message"=>$e->getMessage(),
+            ]);
+        }
+    }
+
 }
