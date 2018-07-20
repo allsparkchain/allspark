ea64f4baec996581fc66606457f43524e60d684c
shixiaoyi <729718933@qq.com>
2018-07-20 15:45:33 +0800

    no message

---
+++ shop/views/H5/404.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+get_header(); ?>
+<div class="wrap">
+	<section class="allspark404 not-found">
+		<img src="/images/404.png" alt="404">
+		<p>Error 404: Not Found</p>
+		<p><a href="/">Home Back</a></p>
+	</section>
+</div><!-- .wrap -->
+<div class="allFooterWrap">
+	<div class="allFooter clearfix">
+		<div class="footLt fl">
+			<h4>CONNECT WITH US</h4>
+			<div class="footPR"><a href="mailto:hwang@allsparkchain.org">KOREAN SUPPORT: hwang@allsparkchain.org</a></div>
+			<div class="footCopy">2018 ALLSPARKCHAIN.COM, ALL RIGHTS RESERVED</div>
+		</div>
+		<div class="spaPartner fr">
+			<a target="_blank" href="https://www.facebook.com/allsparkchain/" class="a1"></a>
+			<a target="_blank" href="https://twitter.com/allsparkchain" class="a2"></a>
+			<a target="_blank" href="https://github.com/allsparkchain" class="a3"></a>
+			<a target="_blank" href="https://t.me/allsparkchat" class="a4"></a>
+		</div>
+	</div>
+</div>
+
